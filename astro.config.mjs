2a8cecadea63e0c5d75ccd4d0a8c0b56a5063bc1
nemutas <inohiro.focru@gmail.com>
2023-03-11 17:17:08 +0900

    init

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,8 @@ import glsl from 'vite-plugin-glsl'
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://nemutas.github.io',
+  base: '/three-template-with-astro',
   vite: {
     plugins: [glsl()],
     build: {
